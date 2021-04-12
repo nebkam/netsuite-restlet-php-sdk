9c020cbc7ae402ae5a3f4dfb91e57c372296b017
Silvester Kokai <silvester.kokai@infostud.com>
2021-04-12 09:54:34 +0200

    More test fixes

--- tests/ApiServiceTest.php
+++ tests/ApiServiceTest.php
@@ -211,6 +211,7 @@ class ApiServiceTest extends TestCase
 			->setDepartment(getenv('DEPARTMENT_ID'))
 			->setLocation(getenv('LOCATION_ID'))
 			->setClassification(getenv('CLASSIFICATION_ID'))
+			->setType(SalesOrderForm::TYPE_NONE)
 			->setCustomer($customerId)
 			->addItem(
 				(new SalesOrderItem())
